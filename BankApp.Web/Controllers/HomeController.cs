aca15346def76dcf7c323344a5d45d78f690059c
Samet ALTUNDAĞ <smtaltundag63@gmail.com>
2024-08-04 16:29:37 +0300

    Proje katmanları oluşturuldu ve EfCore entegre edildi.

---
+++ BankApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using BankApp.Web.Data.Context;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace BankApp.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly BankContext _context;
+
+        public HomeController(BankContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View(_context.ApplicationUsers.ToList());
+        }
+    }
+}
